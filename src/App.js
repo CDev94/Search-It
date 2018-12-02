352ccdbc5570de106b9a1095021bfa8dc27079b0
Curdarious Bell <BellCurdarious@yahoo.com>
2018-12-01 23:36:52 -0600

    added axios for data fetching

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import './App.scss';
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
-
+import './App.scss';
 class App extends Component  {
     
     // Collecting input data from SearchBar
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/photos/?client_id=5352a9d9c2a176375fd5544d01476fe1dfd9dc490b95c97e7a329c1026966efe')
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
     }
  
 
