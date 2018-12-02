781a67ca1b2ca43cbfd8726265c63b86c3a1c07f
Curdarious Bell <BellCurdarious@yahoo.com>
2018-12-01 23:35:43 -0600

    removed comment

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -19,7 +19,7 @@ class SearchBar extends Component {
      <form className="ui form" onSubmit={this.onFormSubmit}>
      <div className="field">
      <label>Image Search</label>
-      //  Collecting the input value and setting the term state 
+
       <input type="text" value={this.state.term} onChange={e => this.setState({ term: e.target.value})} />
       </div>
       </form>
